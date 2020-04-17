82ef8ce219e8dfe5de3cb87e0c5fcaa694411325
yosukekato165 <padyk165@gmail.com>
2020-04-17 19:27:43 +0900

    chenge text on gatsby-config.js and .env.develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,8 +42,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: ghnimbz99n3y,
+        accessToken: IpMRYYlki41uuVTlpxHBIve_csA68kaG_IJwoo4B0GM,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

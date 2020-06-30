ff2b738efd8f7a0e3fe9909bffbf66d0a85b2159
yosukekato165 <padyk165@gmail.com>
2020-06-30 15:45:04 +0900

    pages/index.jsのimportにLinkが無かったので追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BlogList from "../components/blog-list"
+import { Link } from "gatsby"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
     <BlogList/>
-    <Link to="/blog/">Go to Blog</Link>
+    <Link to="/blog/">Go to Blog </Link>
   </Layout>
 )
 
